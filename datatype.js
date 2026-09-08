6876b7c5927895950d532799e047ba57f8ef92fe
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-08 16:10:51 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,32 @@
+// let name="vikas";
+
+// let age=22;
+// //number datatype
+// console.log(typeof(age));
+
+// let s=true;
+// let c;
+// let address=null;
+// //null datatype
+
+// let x=10;
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
+// ++x;
+// console.log(x);
+// --x;
+// console.log(x);
+// x+=2;
+// console.log(x);
+
+const student={
+    name:"vikas",
+    age:22,
+    city:"delhi"
+};
+console.log(student.name);
+console.log(student.age);
+console.log(student.city);
+
