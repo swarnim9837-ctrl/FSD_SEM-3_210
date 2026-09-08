02017fbf038f718b65cf90dbb59f64b4d2110cbb
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-08 16:11:50 +0530

    Add files via upload

---
+++ middlelevel.js
@@ -0,0 +1,22 @@
+//advance
+
+//scope
+// let globalscope;
+
+// let college = "ABES Engineering college";
+
+// function showCollege() {
+//     console.log(college);
+// }
+
+// showCollege();
+
+
+//function scope
+
+//block scope
+if (true) {
+    let a=10;
+    console.log(a);
+}
+
