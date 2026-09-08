b9fc16f28e99aab430665de5141af6e52b1290da
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-08 16:12:19 +0530

    Add files via upload

---
+++ rest_operator.js
@@ -0,0 +1,26 @@
+//rest operator
+
+// function sum(...numbers) {
+//     let total = 0;
+
+//     for (let num of numbers) {
+//         total += num;
+//     }
+
+//     return total;
+// }
+
+// console.log(sum(10, 20, 30, 40));
+
+
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(add(10, 20));
+
+
+
+
+
+
