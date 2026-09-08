f9d07c1b67d0f6379d24af6a2a21cd4fb94497cb
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-08 16:10:35 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,69 @@
+// let a=[10,20,30,40];
+// a.push(50);
+// console.log(a); 
+// a.pop();
+// console.log(a);
+// a.unshift(5);
+// console.log(a);
+// a.shift();
+// console.log(a);
+// console.log(a.indexOf(30));
+// console.log(a.includes(40));
+
+
+
+
+//for of
+// let fruits=["apple","banana","mango","grapes"];
+// for(const fruit of fruits){
+//     console.log(fruit);  
+// }
+
+
+
+
+//for of
+// let fruits=["apple","banana","mango","grapes"];
+// for(const fruit of fruits){
+//     console.log(fruit);  
+// }
+//for in
+// let student={
+//     name:"sam",
+//     age:20,
+//     city:"delhi"
+// };
+// for(let key in student){
+//     console.log(key+": "+student[key]);
+// }
+
+
+//Array Destructuring
+// let marks=[10,20,30];
+// let [java,python,cpp]=marks;
+// console.log(java);
+// console.log(python);
+// console.log(cpp);
+
+
+
+// let arr1=[10,20,30];
+// let arr2=[40,50,60];
+// let arr3=[...arr1,...arr2];
+// console.log(arr3);
+// // spread operator in object
+// let obj1={
+//     name:"sonali",
+//     age:19
+// };
+// let obj2={
+//     ...obj1,
+//     city:"gorakhpur",
+//     country:"india"
+// };
+// console.log(obj2);
+
+
+
+
+
