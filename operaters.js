da906b7999fb3120a2df97ae2e24548f9792539b
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-08 16:12:05 +0530

    Add files via upload

---
+++ operaters.js
@@ -0,0 +1,96 @@
+//loops
+
+//for
+//while
+//do-while
+
+// let i = 0;
+
+// for (; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+// for(let j=1; ;j+=2){
+//     console.log(j);
+//     if(j>10){
+//         break;
+//     }
+// }
+
+
+// let j=1;
+// for(;;){
+//     if(j>10){
+//         break;
+//     }
+//     console.log(j);
+//     j+=2;
+// }
+
+
+
+// let sum=0;
+// for(let i=0;i<=9;i++,sum+=i)
+// console.log(sum);
+
+
+// for(i=0;i<5;i++){
+//     if(i==2){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+// let x=4,y=0,z;
+// while(x>=0){
+//     x--;
+//     y++;
+//     if(x==y){
+//         break;
+//     }else{
+//         console.log(x,y)
+//     }
+// }
+
+// let x=4,y=0,z;
+// while(x>=0){
+//     x--;
+//     y++;
+//     if(x==y){
+//         continue;
+//     }else{
+//         console.log(x,y)
+//     }
+// }
+
+
+
+
+//comma operator
+
+// let arr = [10,20,30,40,10];
+// for(let i=0,j=arr.length-1;i<j;i++,j--){
+//     console.log(arr[i],arr[j]);
+// }
+
+
+
+// if(true){
+//     var name="John";
+//     console.log(name);
+// }
+// console.log(name);
+
+
+// function start(){
+//     var name="John";
+//     if(true){
+//         var name="Doe";
+//         console.log(name);
+//     }
+// }
+
+
