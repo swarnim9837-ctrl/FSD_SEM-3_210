03dbd5bf3d8a142a5ab6fbfa58d947ba4eaa2ce3
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-08 16:11:10 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,52 @@
+// let year = 2024;
+
+// if (year % 4 === 0) {
+//     if (year % 100 === 0) {
+//         if (year % 400 === 0) {
+//             console.log("Leap Year");
+//         } else {
+//             console.log("Not a Leap Year");
+//         }
+//     } else {
+//         console.log("Leap Year");
+//     }
+// } else {
+//     console.log("Not a Leap Year");
+// }
+
+
+// let year=2024;
+
+// if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+//     console.log("Leap Year");
+// else
+//     console.log("Not a Leap Year");
+
+
+
+// let a = 10, b = 5;
+
+// console.log(a > 0 && b > 0); 
+// console.log(a > 0 || b > 0); 
+// console.log(!(a > 0)); 
+
+let f=["apple","banana","orange"];
+f.pop();
+console.log(f);
+
+str="Hello, World!";
+console.log(str.length);
+console.log(str.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
