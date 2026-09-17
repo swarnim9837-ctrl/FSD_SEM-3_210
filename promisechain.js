86ae15acfa6a6f92964595f1b90c780bc9864929
swarnim9837-ctrl <swarnim9837@gmail.com>
2026-09-17 14:01:57 +0530

    Add files via upload

---
+++ promisechain.js
@@ -0,0 +1,15 @@
+function step1(){
+    return Promise.resolve(10);
+}
+step1().then((result)=>{
+    console.log('step 1',result);
+    return result+10;
+}).then((result)=>{
+    console.log('step 2',result);
+    return result+10;
+}).then((result)=>{
+    console.log('step 3',result);
+    return result+10;
+}).catch((error)=>{
+    console.log('error',error);
+});
